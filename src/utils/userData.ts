4e47fca5a26bad10b3b5cae9168a439377b3bb76
Enrique <enrique.barbosasilva@gmail.com>
2023-01-17 17:10:26 -0300

    refactor: code-changed

--- src/utils/userData.ts
+++ src/utils/userData.ts
@@ -1,7 +1,7 @@
 export const userData = {
   nameUser: "Enrique Barbosa",
   githubUser: "enriquebds",
-  linkedinUser: "nekelpatrick",
+  linkedinUser: "enrique-barbosa-6b3aaa22b",
   whatsappNumber: "+5541988819028",
   emailUser: "samuel@kenzie.com.br",
   instagramUser: "samuelleaoui",
